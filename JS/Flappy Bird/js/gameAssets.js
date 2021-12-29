0aef5ea0a1532eda4d1e2146c39a9dc2024cbc2d
Binit Ghimire <thebinitghimire@gmail.com>
2021-12-30 03:36:13 +0545

    Fixed path for game assets!

--- JS/Flappy Bird/js/gameAssets.js
+++ JS/Flappy Bird/js/gameAssets.js
@@ -1,16 +1,16 @@
 let treeImg = new Image();
 let treeRImg = new Image();
 
-treeImg.src = "../images/tree.png";
-treeRImg.src = "../images/treeR.png";
+treeImg.src = "./images/tree.png";
+treeRImg.src = "./images/treeR.png";
 
 const available = [treeImg, treeRImg];
 
 let woodsImg = new Image(260, 84);
-woodsImg.src = "../images/woods.png";
+woodsImg.src = "./images/woods.png";
 
 let birdImg = new Image();
-birdImg.src = "../images/bird.png";
+birdImg.src = "./images/bird.png";
 
 let scorecardImg = new Image();
-scorecardImg.src = "../images/scorecard.png";
+scorecardImg.src = "./images/scorecard.png";
